4cd1791e944b72ec3c0a5ce0f9f1c0a549f2c0c9
shawn_dad <13910082361@139.com>
2026-06-04 22:43:35 +0800

    Initial Commit

---
+++ IslandPulse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IslandPulse
+//
+//  Created by 朱建鑫 on 2026/6/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IslandPulse/IslandPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IslandPulseApp.swift
+//  IslandPulse
+//
+//  Created by 朱建鑫 on 2026/6/4.
+//
+
+import SwiftUI
+
+@main
+struct IslandPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
